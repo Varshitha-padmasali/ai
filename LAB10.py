dd03e68591d8aee313b53e6e72f04aef30216eea
Varshitha-2509 <jinkavarshithapadmasali@gmail.com>
2026-05-22 20:40:35 +0530

    Resolution code changed

--- LAB10.py
+++ LAB10.py
@@ -1,38 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def resolve(clause1, clause2):
     resolved = []
     for literal in clause1:
@@ -41,7 +6,7 @@ def resolve(clause1, clause2):
             new_clause.remove(literal)
             new_clause.remove(literal[1:])
             resolved.append(new_clause)
-        elif ('~' + literal) in clause2:#open bracket
+        elif ('~' + literal) in clause2:
             new_clause = list(set(clause1 + clause2))
             new_clause.remove(literal)
             new_clause.remove('~' + literal)
@@ -49,21 +14,21 @@ def resolve(clause1, clause2):
     return resolved
 def resolution(kb, query):
     neg_query = ['~' + query]
-    kb.append(neg_query) #append
-    new = [] ####
+    kb.append(neg_query) 
+    new = []
     while True:
-        n = len(kb) #####
+        n = len(kb) 
         for i in range(n):
             for j in range(i + 1, n):
-                resolvents = resolve(kb[i], kb[j]) ####
-                for r in resolvents: #####
+                resolvents = resolve(kb[i], kb[j]) 
+                for r in resolvents: 
                     if r == []:
                         return True
-                    if r not in kb and r not in new: ####
+                    if r not in kb and r not in new: 
                         new.append(r)
-        if not new: ####
+        if not new: 
             return False
-        kb.extend(new) ###extend
+        kb.extend(new) 
 knowledge_base = [
     ['P', 'Q'],
     ['~P', 'R'],

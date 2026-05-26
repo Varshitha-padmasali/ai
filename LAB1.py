50e811a02a4829463da001032a3588f6d2f90bca
Varshitha-2509 <jinkavarshithapadmasali@gmail.com>
2026-05-26 16:51:46 +0530

    is_safe function removed

--- LAB1.py
+++ LAB1.py
@@ -7,23 +7,6 @@ def print_solution(board,n):
                 print(".",end=" ")
         print()
     print()
-def is_safe(board,row,col,n):
-    for i in range(col):
-        if board[row][i]==1:
-            return False
-    i,j=row,col
-    while i>=0 and j>=0:
-        if board[i][j]==1:
-            return False
-        i-=1
-        j-=1
-    i,j=row,col
-    while i<n and j>=0:
-        if board[i][j]==1:
-            return False
-        i+=1
-        j-=1
-    return True
 def solve_nqueens(board,col,n):
     if col>=n:
         print_solution(board,n)
